c480a82461b0422cb7809430ca78ee0967de0cf0
DRAGGAME <maxutnov@mail.ru>
2024-12-04 21:35:19 +0300

    final 0.2

--- db/db_gino.py
+++ db/db_gino.py
@@ -1,7 +1,4 @@
-import asyncio
-
 import psycopg2
-from aiogram.client import bot
 from psycopg2 import sql
 from psycopg2 import Error
 
@@ -17,28 +14,116 @@ class Sqlbase:
         self.cursor = self.connection.cursor()
 
 
-    async def notify_listener():
-        conn = await asyncpg.connect(**DB_PARAMS)
-        await conn.add_listener('new_review', handle_new_review)
-        print("Слушаем канал new_review...")
+    def spaltenerstellen(self):
+        # Создание столбцов
+        try:
+            self.cursor.execute('''CREATE TABLE IF NOT EXISTS adm(
+                                Id SERIAL PRIMARY KEY,
+                                Adm_one text,
+                                Adm_too text,
+                                Adm_three text
+                                );''')
+            self.connection.commit()
+        except Error as e:
+            # Откат изменений в случае ошибки
+            self.connection.rollback()
+            # Выводим сообщение об ошибке и ее код
+            print(f"Transaction failed: {e.pgcode} - {e.pgerror}")
 
-    async def handle_new_review(connection, pid, channel, payload):
-        # payload содержит данные из pg_notify
-        print(f"Получено уведомление: {payload}")
-        # Отправляем сообщение в Telegram
-        await bot.send_message(chat_id="your_chat_id", text=f"Новый отзыв: {payload}")
+        finally:
+            if self.connection:
+                self.cursor.close()
+                self.connection.close()
+                print('Всё окей')
 
-    async def main():
-        # Запускаем слушатель уведомлений и бота
-        await asyncio.gather(notify_listener(), dp.start_polling(bot))
+
+    def spaltenausgabe(self):
+        try:
+            query = sql.SQL("SELECT {fields} FROM {table} WHERE {pkey} = %s;").format(
+                    fields=sql.SQL(', ').join([
+                        sql.Identifier('id'),
+                        sql.Identifier('data_times'),
+                        sql.Identifier('place'),
+                        sql.Identifier('id_user'),
+                        sql.Identifier('rating'),
+                        sql.Identifier('review')
+                    ]),
+                    table=sql.Identifier('servers'),
+                    pkey=sql.Identifier('id')
+                )
+            self.cursor.execute(query, (1,))
+            self.connection.commit()
+            print(self.cursor.fetchone())
+        except Error as e:
+            # Откат изменений в случае ошибки
+            self.connection.rollback()
+            # Выводим сообщение об ошибке и ее код
+            print(f"Transaction failed: {e.pgcode} - {e.pgerror}")
+
+        finally:
+            if self.connection:
+                self.cursor.close()
+                self.connection.close()
+                print('Всё окей')
+    '''time, place, user, rating, review, - При вставке в таблицу servers. Если для получения, таблица adm'''
+    def ins(self, adm1, adm2, adm3):
+        try:
+            self.cursor.execute("INSERT INTO servers (adm_one, adm_too, adm_three) VALUES (%s, %s, %s);", (adm1, adm2, adm3))
+
+            self.cursor.execute(f"NOTIFY {PG_user}, 'Данные обновлены';")
+            self.connection.commit()
+        except Error as e:
+            # Откат изменений в случае ошибки
+            self.connection.rollback()
+            # Выводим сообщение об ошибке и ее код
+            print(f"Transaction failed: {str(e)}")
 
         # finally:
         #     if self.connection:
         #         self.cursor.close()
         #         self.connection.close()
         #         print('Всё окей')
+
+    def selectes(self):
+        try:
+            with self.connection.cursor() as cur:
+                cur.execute(f"LISTEN {PG_user};")
+                print("Ожидание уведомлений о новых данных...")
+                cur.execute(f"SELECT adm_one, adm_too, adm_three FROM adm ORDER BY id DESC LIMIT 1;")
+                rows = cur.fetchall()
+                # print("Последние данные в таблице:")
+                for row in rows:
+                    nice = row
+                    nice = list(nice)
+                return nice
+
+
+        except Error as e:
+            # Откат изменений в случае ошибки
+            self.connection.rollback()
+            # Выводим сообщение об ошибке и ее код
+            print(f"Transaction failed: {str(e)}")
+
+    def delete(self):
+        try:
+            self.cursor.execute("DROP TABLE adm;")
+            self.connection.commit()
+
+        except Error as e:
+            # Откат изменений в случае ошибки
+            self.connection.rollback()
+            # Выводим сообщение об ошибке и ее код
+            print(f"Transaction failed: {str(e)}")
+
+        finally:
+            if self.connection:
+                self.cursor.close()
+                self.connection.close()
+                print('Всё круто')
 if __name__ == '__main__':
 
     test_sql_class = Sqlbase()
-
-    test_sql_class.spaltenausgabe()
+    test_sql_class.selectes()
+    # test_sql_class.delete()
+    # test_sql_class.spaltenerstellen()
+    # test_sql_class.ins('2005683766', 'None', 'None')
--- run.py
+++ run.py
@@ -12,32 +12,35 @@ from aiogram import Bot, Dispatcher
 from psycopg2 import sql
 import select
 from pyexpat.errors import messages
-
 from config import ip, PG_password, PG_user, DATABASE
 
 logging.basicConfig(level=logging.INFO)
 connection = psycopg2.connect(host=ip, user=PG_user, password=PG_password, database=DATABASE)
 cursor = connection.cursor()
 load_dotenv()
-global chatid
-bot = Bot(token= os.getenv('API_KEY'))
+bot = Bot(token=os.getenv('API_KEY'))
 dp = Dispatcher()
 
 
 
-
-#Если только по старту, в таком случае вывод всего
 async def start_cmd():
-    global chatid
     connection.autocommit = True
- # Для получения уведомлений
+    # Для получения уведомлений
     try:
         with connection.cursor() as cur:
             # Подписываемся на канал уведомлений
             cur.execute(f"LISTEN {PG_user};")
             print("Ожидание уведомлений о новых данных...")
+            cur.execute(f"SELECT adm_one, adm_too, adm_three FROM adm ORDER BY id DESC LIMIT 1;")
+            rows = cur.fetchall()
+            # print("Последние данные в таблице:")
+            for row in rows:
+                nice = row
+                nice = list(nice)
+                print(nice)
 
             while True:
+
                 # Ожидание уведомлений
                 if select.select([connection], [], [], 5) == ([], [], []):
                     pass
@@ -49,27 +52,36 @@ async def start_cmd():
                         # print(f"Уведомление получено: {notify.payload}")
 
                         # Получаем последние данные из таблицы
-                        cur.execute(f"SELECT Data_times, Place, Id_user, Rating, Review FROM servers ORDER BY id DESC LIMIT 1;")
+                        cur.execute(
+                            f"SELECT Data_times, Place, Id_user, Rating, Review FROM servers ORDER BY id DESC LIMIT 1;")
                         rows = cur.fetchall()
                         # print("Последние данные в таблице:")
                         for row in rows:
-                            print(type(row))
-                            await bot.send_message(chat_id='2005683766', text=f'Дата, Место, Пользователь, Отзыв\n{row}')
+                            row = list(row)
+                            # print(type(row))
+                            for i in range(3):
+                                if nice[i] in 'None':
+                                    pass
+                                else:
+                                    await bot.send_message(chat_id=nice[i], text=f'Дата: {row[0]}\nМесто: {row[1]} \nПользователь(id): {row[2]}\nРейтинг: {row[3]} \nОтзыв: {row[4]}')
 
+                            #Подумать нунна ли cmd_start и сделать в row try и expect. Текущая цельЖ Обозначить админов автоматически
     except Error as e:
         # Откат изменений в случае ошибки
         connection.rollback()
         # Выводим сообщение об ошибке и ее код
         print(f"Transaction failed: {str(e)}")
 
+
+
 async def start_c():
     await start_cmd()
+
 
 async def main():
     await asyncio.create_task(start_cmd())
 
     await dp.start_polling(bot)
-
 
 
 if __name__ == '__main__':
